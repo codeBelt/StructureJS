bb6989d82cbf198dfa4094708939c98c38d2cfe3
codeBelt <code@codebelt.com>
2015-04-21 21:42:28 -0500

    Add event listeners to MovieCollection example

--- examples/MovieCollection/src/assets/scripts/view/PageControlView.js
+++ examples/MovieCollection/src/assets/scripts/view/PageControlView.js
@@ -79,6 +79,10 @@ define(function (require, exports, module) { // jshint ignore:line
             this._$listLimit.prop('disabled', false);
             this._$listUpdate.prop('disabled', false);
 
+            this._$listSort.addEventListener('change', this._onSortChange, this);
+            this._$listLimit.addEventListener('change', this._onLimitChange, this);
+            this._$listUpdate.addEventListener('click', this._onUpdateClick, this);
+
             return _super.prototype.enable.call(this);
         };
 
@@ -93,17 +97,51 @@ define(function (require, exports, module) { // jshint ignore:line
             this._$listLimit.prop('disabled', true);
             this._$listUpdate.prop('disabled', true);
 
+            this._$listSort.removeEventListener('change', this._onSortChange, this);
+            this._$listLimit.removeEventListener('change', this._onLimitChange, this);
+            this._$listUpdate.removeEventListener('click', this._onUpdateClick, this);
+
             return _super.prototype.disable.call(this);
         };
 
-        /**
-         * @overridden DOMElement.destroy
-         */
-        PageControlView.prototype.destroy = function () {
-            // Call destroy on any child objects that is need.
-            // This super method will also null out all properties automatically to prevent memory leaks.
 
-            _super.prototype.destroy.call(this);
+        /**
+         * TODO: YUIDoc_comment
+         *
+         * @method _onSortChange
+         * @param event {jQueryEventObject}
+         * @private
+         */
+        PageControlView.prototype._onSortChange = function(event) {
+            event.preventDefault();
+
+            console.log("_onSortChange");
+        };
+
+        /**
+         * TODO: YUIDoc_comment
+         *
+         * @method _onLimitChange
+         * @param event {jQueryEventObject}
+         * @private
+         */
+        PageControlView.prototype._onLimitChange = function(event) {
+            event.preventDefault();
+
+            console.log("_onLimitChange");
+        };
+
+        /**
+         * TODO: YUIDoc_comment
+         *
+         * @method _onUpdateClick
+         * @param event {jQueryEventObject}
+         * @private
+         */
+        PageControlView.prototype._onUpdateClick = function(event) {
+            event.preventDefault();
+
+            console.log("_onUpdateClick");
         };
 
         return PageControlView;
