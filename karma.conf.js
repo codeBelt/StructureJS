03e0ebf25fb4351050d8e995592f62f6d199e9e1
Robert S <rsavian@nerdery.com>
2015-05-12 15:32:57 -0500

    update tests

--- karma.conf.js
+++ karma.conf.js
@@ -1,67 +1,76 @@
-// Karma configuration
-// Generated on Sun Oct 27 2013 01:56:27 GMT+0200 (CEST)
-
-module.exports = function (config) {
-
+module.exports = function(config)
+{
     config.set({
 
-            // base path, that will be used to resolve files and exclude
-            basePath: '.',
+        // base path, that will be used to resolve files and exclude
+        basePath: '',
 
 
-            // frameworks to use
-            frameworks: ['requirejs', 'jasmine'],
+        // frameworks to use
+        frameworks: ['browserify', 'jasmine'],
 
 
-            // list of files / patterns to load in the browser
-            files: [
-                'src/config.js',
-                {pattern: 'src/**/*.js', included: false},
-                {pattern: 'test/**/*Spec.js', included: false},
-                'test/_karma.js'
-            ],
+        // list of files / patterns to load in the browser
+        files: [
+            'tests/*.js'
+        ],
 
 
-            // test results reporter to use
-            // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-            reporters: ['dots'],
+        // list of files to exclude
+        exclude: [
+        ],
+
+        preprocessors: {
+            'tests/*.js': ['browserify']
+        },
+
+//        // test results reporter to use
+//        // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+//        reporters: ['progress'],
 
 
-            // web server port
-            port: 9876,
+        // web server port
+        port: 9876,
 
 
-            // enable / disable colors in the output (reporters and logs)
-            colors: true,
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
 
-            // level of logging
-            // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-            logLevel: config.LOG_WARN,
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
 
-            // enable / disable watching file and executing tests whenever any file changes
-            autoWatch: true,
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: false,
 
 
-            // Start these browsers, currently available:
-            // - Chrome
-            // - ChromeCanary
-            // - Firefox
-            // - Opera (has to be installed with `npm install karma-opera-launcher`)
-            // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
-            // - PhantomJS
-            // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
-            browsers: ['PhantomJS'/*, 'Chrome'*/],
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera (has to be installed with `npm install karma-opera-launcher`)
+        // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
+        // - PhantomJS
+        // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
+        browsers: ['PhantomJS'],
+
+        browserify: {
+            debug: true,
+            transform: []
+        },
+
+        plugins: [
+            'karma-phantomjs-launcher',
+            'karma-jasmine','karma-bro'],
 
 
-            // If browser does not capture in given timeout [ms], kill it
-            captureTimeout: 60000,
+        // If browser does not capture in given timeout [ms], kill it
+        captureTimeout: 60000,
 
-
-            // Continuous Integration mode
-            // if true, it capture browsers, run tests and exit
-            singleRun: false
-        }
-    );
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: true
+    });
 };
