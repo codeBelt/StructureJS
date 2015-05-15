f2a13134773656e2a004ce2fe57df31d2b9c0846
codeBelt <code@codebelt.com>
2015-05-15 16:25:40 -0500

    fix unit test.

--- test/model/valueobjects/DotNetAbstractVO.js
+++ test/model/valueobjects/DotNetAbstractVO.js
@@ -1,92 +1,89 @@
-define(function (require, exports, module) { // jshint ignore:line
-    'use strict';
+'use strict';
 
-    // Imports
-    var Extend = require('structurejs/util/Extend');
-    var ValueObject = require('structurejs/model/ValueObject');
-    var StringUtil = require('structurejs/util/StringUtil');
+// Imports
+var Extend = require('../../../js/util/Extend');
+var ValueObject = require('../../../js/model/ValueObject');
+var StringUtil = require('../../../js/util/StringUtil');
+
+/**
+ * TODO: YUIDoc_comment
+ *
+ * @class DotNetAbstractVO
+ * @extends ValueObject
+ * @constructor
+ **/
+var DotNetAbstractVO = (function () {
+
+    var _super = Extend(DotNetAbstractVO, ValueObject);
+
+    function DotNetAbstractVO(data) {
+        _super.call(this);
+
+        this.name = null;
+        this.age = null;
+        this.dateOfBirth = null;
+
+        if (data) {
+            this.update(data);
+        }
+    }
 
     /**
-     * TODO: YUIDoc_comment
+     * @overridden ValueObject.update
+     */
+    DotNetAbstractVO.prototype.update = function (data) {
+        var camelCaseVersion;
+
+        for (var key in data)
+        {
+            camelCaseVersion = StringUtil.toCamelCase(key);
+
+            this._setData(camelCaseVersion, data[key]);
+        }
+
+        this.dateOfBirth = new Date(data.DateOfBirth);
+
+        return this;
+    };
+
+    /**
+     * YUIDoc_comment
      *
-     * @class DotNetAbstractVO
-     * @extends ValueObject
-     * @constructor
-     **/
-    var DotNetAbstractVO = (function () {
+     * @method toPascalCaseJSON
+     * @public
+     */
+    DotNetAbstractVO.prototype.toPascalCaseJSON = function() {
+        return this._toPascalCase(this.toJSON());
+    };
 
-        var _super = Extend(DotNetAbstractVO, ValueObject);
+    /**
+     * YUIDoc_comment
+     *
+     * @method _toPascalCase
+     * @private
+     */
+    DotNetAbstractVO.prototype._toPascalCase = function(obj) {
+        var newObj = {};
+        var pascalCaseVersion;
 
-        function DotNetAbstractVO(data) {
-            _super.call(this);
+        for (var key in obj)
+        {
+            pascalCaseVersion = StringUtil.toPascalCase(key);
 
-            this.name = null;
-            this.age = null;
-            this.dateOfBirth = null;
-
-            if (data) {
-                this.update(data);
+            if (obj.hasOwnProperty(key))
+            {
+                if (obj[key] instanceof Object) {
+                    newObj[pascalCaseVersion] = this._toPascalCase(obj[key]);
+                } else {
+                    newObj[pascalCaseVersion] = obj[key];
+                }
             }
         }
 
-        /**
-         * @overridden ValueObject.update
-         */
-        DotNetAbstractVO.prototype.update = function (data) {
-            var camelCaseVersion;
+        return newObj;
+    };
 
-            for (var key in data)
-            {
-                camelCaseVersion = StringUtil.toCamelCase(key);
+    return DotNetAbstractVO;
+})();
 
-                this._setData(camelCaseVersion, data[key]);
-            }
-
-            this.dateOfBirth = new Date(data.DateOfBirth);
-
-            return this;
-        };
-
-        /**
-         * YUIDoc_comment
-         *
-         * @method toPascalCaseJSON
-         * @public
-         */
-        DotNetAbstractVO.prototype.toPascalCaseJSON = function() {
-            return this._toPascalCase(this.toJSON());
-        };
-
-        /**
-         * YUIDoc_comment
-         *
-         * @method _toPascalCase
-         * @private
-         */
-        DotNetAbstractVO.prototype._toPascalCase = function(obj) {
-            var newObj = {};
-            var pascalCaseVersion;
-
-            for (var key in obj)
-            {
-                pascalCaseVersion = StringUtil.toPascalCase(key);
-
-                if (obj.hasOwnProperty(key))
-                {
-                    if (obj[key] instanceof Object) {
-                        newObj[pascalCaseVersion] = this._toPascalCase(obj[key]);
-                    } else {
-                        newObj[pascalCaseVersion] = obj[key];
-                    }
-                }
-            }
-
-            return newObj;
-        };
-
-        return DotNetAbstractVO;
-    })();
-
-    module.exports = DotNetAbstractVO;
-
-});
+module.exports = DotNetAbstractVO;
