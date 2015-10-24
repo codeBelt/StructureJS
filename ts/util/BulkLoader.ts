37d717d8e6e8691465ad71aa66159a8c7d348866
codeBelt <code@codebelt.com>
2015-10-24 13:18:57 -0500

    Update BulkLoader and create BulkLoaderEvent.

--- ts/util/BulkLoader.ts
+++ ts/util/BulkLoader.ts
@@ -1,93 +1,174 @@
 import IDataStore = require('../interface/IDataStore');
 import EventDispatcher = require('../event/EventDispatcher');
 import LoaderEvent = require('../event/LoaderEvent');
+import BulkLoaderEvent = require('../event/BulkLoaderEvent');
+import Collection = require('../model/Collection');
 
 /**
- * The BulkLoader...
+ * TODO: YUIDoc_comment
  *
  * @class BulkLoader
+ * @extends EventDispatcher
  * @module StructureJS
  * @submodule util
  * @requires Extend
- * @requires EventDispatcher
  * @requires LoaderEvent
  * @constructor
  * @author Robert S. (www.codeBelt.com)
  */
-class BulkLoader {
-
-    private static _dataStores:Array<IDataStore> = [];
-    private static _eventDispatcher:EventDispatcher = new EventDispatcher();
+class BulkLoader extends EventDispatcher
+{
+    protected _dataStores:Collection = null;
+    protected _totalComplete:number = 0;
 
     constructor()
     {
-        throw new Error('[BulkLoader] Do not instantiate the BulkLoader class because it is a static class.');
+        super();
+
+        this._dataStores = new Collection();
     }
 
-    public static addFile(dataStore:IDataStore, key:string):void
+    /**
+     * TODO: YUIDoc_comment
+     *
+     * @method addFile
+     * @param dataStore {IDataStore}
+     * @param key {string}
+     * @public
+     */
+    public addFile(dataStore:IDataStore, key:string = null):void
     {
-        BulkLoader._dataStores[key] = dataStore;
+        if (key === null)
+        {
+            key = String(dataStore.sjsId);
+        }
+
+        var model = {
+            key: key,
+            value: dataStore
+        };
+
+        this._dataStores.add(model);
     }
 
-    public static getFile(key:string):IDataStore
+    /**
+     * TODO: YUIDoc_comment
+     *
+     * @method getFile
+     * @param key {string}
+     * @public
+     */
+    public getFile(key:string):IDataStore
     {
-        return BulkLoader._dataStores[key] || null;
+        var model = this._dataStores.findBy({key: key})[0];
+        return model.value;
     }
 
-    public static getImage(key:string):HTMLImageElement
+    /**
+     * TODO: YUIDoc_comment
+     *
+     * @method getImage
+     * @param key {string}
+     * @return {Image}
+     * @public
+     */
+    public getImage(key:string):HTMLImageElement
     {
-        var imageLoader:IDataStore = BulkLoader.getFile(key);
+        var imageLoader:IDataStore = this.getFile(key);
         return (imageLoader !== null) ? imageLoader.data : null;
     }
 
-    public static load():void
+    /**
+     * TODO: YUIDoc_comment
+     *
+     * @method start
+     * @public
+     */
+    public start():any
     {
-        for (var key in BulkLoader._dataStores)
+        var dataStore:IDataStore;
+
+        for (var i:number = 0; i < this._dataStores.length; i++)
         {
-            var dataStore:IDataStore = BulkLoader._dataStores[key];
-            dataStore.addEventListener(LoaderEvent.COMPLETE, BulkLoader.onLoadComplete, BulkLoader);
+            dataStore = this._dataStores.get(i).value;
+            dataStore.addEventListenerOnce(LoaderEvent.COMPLETE, this.onLoadComplete, this);
             dataStore.load();
         }
+
+        return this;
     }
 
-    public static addEventListener(type:string, callback:Function, scope:any, priority:number = 0):void {
-        BulkLoader._eventDispatcher.addEventListener(type, callback, scope, priority);
-    }
-
-    public static removeEventListener(type:string, callback:Function, scope:any):void
+    /**
+     * TODO: YUIDoc_comment
+     *
+     * @method clear
+     * @public
+     */
+    public clear():void
     {
-        BulkLoader._eventDispatcher.removeEventListener(type, callback, scope);
-    }
+        var dataStore:IDataStore;
 
-    public static dispatchEvent(type:any, data:any = null):void
-    {
-        var event:any = type;
-
-        if (typeof event === 'string')
+        for (var i:number = 0; i < this._dataStores.length; i++)
         {
-            event = new LoaderEvent(type, data);
+            dataStore = this._dataStores.get(i).value;
+            dataStore.removeEventListener(LoaderEvent.COMPLETE, this.onLoadComplete, this);
         }
 
-        event.target = BulkLoader;
-        event.currentTarget = BulkLoader;
+        this._totalComplete = 0;
 
-        BulkLoader._eventDispatcher.dispatchEvent(event);
+        this._dataStores = new Collection();
     }
 
-    private static onLoadComplete(event:LoaderEvent):void
+    /**
+     * TODO: YUIDoc_comment
+     *
+     * @method onLoadComplete
+     * @param event {LoaderEvent}
+     * @protected
+     */
+    protected onLoadComplete(event:LoaderEvent):void
     {
-        event.target.removeEventListener(LoaderEvent.COMPLETE, BulkLoader.onLoadComplete, BulkLoader);
+        var dataStore:IDataStore = event.target;
 
-        for (var key in BulkLoader._dataStores)
+        this._totalComplete++;
+
+        var bulkLoaderEvent = new BulkLoaderEvent(BulkLoaderEvent.COMPLETE);
+        bulkLoaderEvent.total = this._dataStores.length;
+        bulkLoaderEvent.totalComplete = this._totalComplete;
+        bulkLoaderEvent.percentComplete = (this._totalComplete / this._dataStores.length) * 100;
+        bulkLoaderEvent.data = dataStore;
+
+        // Dispatch the IDataStore that was just completed.
+        this.dispatchEvent(bulkLoaderEvent);
+
+        // Loop through and check if all IDataStore have been loaded.
+        for (var i:number = 0; i < this._dataStores.length; i++)
         {
-            var dataStore:IDataStore = BulkLoader._dataStores[key];
+            dataStore = this._dataStores.get(i).value;
+
             if (dataStore.complete === false)
             {
                 return;
             }
         }
 
-        BulkLoader._eventDispatcher.dispatchEvent(new LoaderEvent(LoaderEvent.LOAD_COMPLETE));
+        var model;
+        var dataStoreList = [];
+        for (var i:number = 0; i < this._dataStores.length; i++)
+        {
+            model = this._dataStores.get(i);
+            dataStoreList.push(model.value);
+        }
+
+        // Add the whole list because all are completed.
+        bulkLoaderEvent = new BulkLoaderEvent(BulkLoaderEvent.LOAD_COMPLETE);
+        bulkLoaderEvent.total = this._dataStores.length;
+        bulkLoaderEvent.totalComplete = this._totalComplete;
+        bulkLoaderEvent.percentComplete = (this._totalComplete / this._dataStores.length) * 100;
+        bulkLoaderEvent.data = dataStoreList;
+        
+        // Only dispatch when all the IDataStore are load complete.
+        this.dispatchEvent(bulkLoaderEvent);
     }
 
 }
