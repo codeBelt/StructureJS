57640ae7ccaea6911b7aff5c5fc9101ec9c33b0b
rsavian <rsavian@nerdery.com>
2016-01-26 17:08:00 -0600

    Update comments

--- ts/util/DateUtil.ts
+++ ts/util/DateUtil.ts
@@ -13,45 +13,56 @@ class DateUtil
      * A list of day names.
      *
      * @property LONG_DAY_LABELS
-     * @type {Array.<any>}
+     * @type [Array=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']]
      * @public
      * @static
      * @final
+     * @example
+     *      DateUtil.LONG_DAY_LABELS[1];
+     *      // 'Monday'
      */
-    public static LONG_DAY_LABELS:Array<any> = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    public static LONG_DAY_LABELS:Array<string> = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
     /**
-     * TODO: YUIDoc_comment
+     * A list of short day names
      *
      * @property SHORT_DAY_LABELS
-     * @type {Array.<any>}
+     * @type [Array=['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']]
      * @public
      * @static
      * @final
+     * @example
+     *      DateUtil.SHORT_DAY_LABELS[1];
+     *      // 'Mon'
      */
-    public static SHORT_DAY_LABELS:Array<any> = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    public static SHORT_DAY_LABELS:Array<string> = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
     /**
      * A list of month names.
      *
      * @property LONG_MONTH_LABELS
-     * @type {Array.<any>}
+     * @type [Array=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']]
      * @public
      * @static
      * @final
+     * @example
+     *      DateUtil.LONG_MONTH_LABELS[1];
+     *      // 'February'
      */
-    public static LONG_MONTH_LABELS:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    public static LONG_MONTH_LABELS:Array<string> = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
     /**
-     * TODO: YUIDoc_comment
+     * A list of short month names.
      *
      * @property SHORT_MONTH_LABELS
-     * @type {Array.<any>}
+     * @type [Array=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']]
      * @public
-     * @static
      * @final
+     * @example
+     *      DateUtil.SHORT_MONTH_LABELS[1];
+     *      // 'Feb'
      */
-    public static SHORT_MONTH_LABELS:Array<any> = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    public static SHORT_MONTH_LABELS:Array<string> = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
     constructor()
     {
