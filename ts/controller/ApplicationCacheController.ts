c8dd8e3a64ee585adb38aa20f409c8d3cbcc9bb3
rsavian <rsavian@nerdery.com>
2016-05-16 11:50:28 -0500

    Remove break; to prevent TypeScript warning.

--- ts/controller/ApplicationCacheController.ts
+++ ts/controller/ApplicationCacheController.ts
@@ -106,25 +106,18 @@ class ApplicationCacheController
         {
             case ApplicationCacheController._appCache.UNCACHED:    // UNCACHED === 0
                 return 'UNCACHED';
-                break;
             case ApplicationCacheController._appCache.IDLE:        // IDLE === 1
                 return 'IDLE';
-                break;
             case ApplicationCacheController._appCache.CHECKING:    // CHECKING === 2
                 return 'CHECKING';
-                break;
             case ApplicationCacheController._appCache.DOWNLOADING: // DOWNLOADING === 3
                 return 'DOWNLOADING';
-                break;
             case ApplicationCacheController._appCache.UPDATEREADY: // UPDATEREADY === 4
                 return 'UPDATEREADY';
-                break;
             case ApplicationCacheController._appCache.OBSOLETE:    // OBSOLETE === 5
                 return 'OBSOLETE';
-                break;
             default:
                 return 'UKNOWN CACHE STATUS';
-                break;
         }
     }
 
